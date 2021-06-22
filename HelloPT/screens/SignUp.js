f6746bfebfb0f714e550b14831caaefaba4ca0db
jin <jjw_0418@naver.com>
2021-06-22 21:05:06 +0900

    *fix: SignUp(회원카입)

---
+++ HelloPT/screens/SignUp.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { StyleSheet, ToastAndroid } from "react-native";
+import Image from '../Images/imgExercise3.jpg'
+import Input from '../components/Input/Input'
+
+const styles=StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+      },
+    textStyles: {
+        marginTop: 10,
+        backgroundColor: '#ffffff',
+        marginBottom: 5,
+        borderRadius: 10
+      },
+      textBlue: {
+        color: "#2e64b0",
+        fontSize: 42,
+        fontWeight: "bold",
+        textAlign: "center",
+      }
+})
+
+function SignUp(props) {
+    const showToast = () => {
+        ToastAndroid.show("아이디 확인이 완료되었습니다.", ToastAndroid.SHORT);
+    };
+    
+    return(
+        <View style={styles.container}>
+            <View style={{ width: '100%', backgroundColor: "#a19f9f", flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                <Text style={styles.textBlue}>회원가입</Text>
+            </View>
+
+            <View style={{ paddingHorizontal: 20 }}>
+
+                <TextInput
+                    placeholder='아이디 입력'
+                    style={styles.textStyles}>
+                  </TextInput>
+
+                <TextInput 
+                  placeholder='비밀번호 입력'
+                  secureTextEntry={true}
+                  style={styles.black, styles.textStyles}>
+                </TextInput>
+                
+                <TextInput 
+                  placeholder='비밀번호 다시 입력'
+                  secureTextEntry={true}
+                  style={styles.black, styles.textStyles}>
+                </TextInput>
+
+                <TextInput 
+                  placeholder='이름 입력'
+                  secureTextEntry={true}
+                  style={styles.black, styles.textStyles}>
+                </TextInput>
+
+                <TextInput 
+                  placeholder='이메일 입력'
+                  secureTextEntry={true}
+                  style={styles.black, styles.textStyles}>
+                </TextInput>
+
+                <View style={{flexDirection: 'row', justifyContent: 'space-around', marginTop: 10}}>
+                  <Text onPress={() => showToast()} style={{fontSize: 20, color: '#7989e8'}}>아이디 중복확인</Text>
+                  <Text
+                      onPress={() => props.navigation.navigate('Login', { userId: 'user', password: '1234' })} style={{fontSize: 20, color: '#7989e8'}}>
+                      회원가입 완료
+                  </Text>
+                </View>
+
+      </View>
+
+        </View>
+    )
+}
+
+export default SignUp
