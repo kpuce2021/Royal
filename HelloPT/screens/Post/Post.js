cf71162c5c82d53ceb3da80c5a2f17d6c68c4902
nwnp-laptop <jjw_0418@naver.com>
2021-09-08 17:07:35 +0900

    fix: db 불러와서 게시판 만들기

--- HelloPT/screens/Post/Post.js
+++ HelloPT/screens/Post/Post.js
@@ -1,12 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Switch, TouchableWithoutFeedback, PermissionsAndroid, FlatList } from 'react-native'
 import Header from '../../components/Header/Header'
 import { styles } from 'ansi-colors'
 import FAB from 'react-native-fab'
 import ActionButton from 'react-native-action-button'
 import Icon from 'react-native-vector-icons/Ionicons'
+import { post } from '../../../Demo/backend/routes/login'
+import axios from 'axios'
+import dayjs from "dayjs";
 
 function Post(props) {
+  const [Lists, setLists] = useState([])
+  useEffect(() => {
+    axios.post('http://10.0.2.2:8080/boards/')
+    .then(res => {
+      console.log('response',res)
+      setLists(res.data)
+    }).catch(err => {
+      console.log('error',err)
+    })
+  },[])
   posts = [
     {id:1, title:"제목입니다.", content:'내용입니다.', time:'26분전', author: '익명'},
     {id:2, title:"제목입니다.", content:'내용입니다.', time:'26분전', author: '익명'},
@@ -18,26 +31,23 @@ function Post(props) {
     {id:8, title:"제목입니다.", content:'내용입니다.', time:'26분전', author: '익명'},
     {id:9, title:"제목입니다.", content:'내용입니다.', time:'26분전', author: '익명'},
     {id:10, title:"제목입니다.", content:'내용입니다.', time:'26분전', author: '익명'},
-
   ]
+
   return(
+    console.log(Lists),
     <View style={{ flex: 1, backgroundColor: '#ffffff' }}>
       <Header navigation={props.navigation} mode="stack" title={props.route.params.title}/>
       <FlatList 
-        data={posts}
-        renderItem={(post)=> <PostList post={post} />}
+        data={Lists}
+        renderItem={(list)=> <PostList post={list} />}
         keyExtractor={(item) => item.id}
       />
-      {/* <FAB 
-      buttonColor="#0099ff" iconTextColor="#FFFFFF"
-      visible={true}
-      /> */}
 
       <ActionButton buttonColor="#c3e0e0">
         <ActionButton.Item
           buttonColor="#affaf9"
           title="게시글 작성"
-          onPress={()=> navigation.navigate('CreatePost')}>
+          onPress={()=> props.navigation.navigate('CreatePost')}>
           <Icon name="add" type="antdesign" />
         </ActionButton.Item>
       </ActionButton>
@@ -49,19 +59,20 @@ function Post(props) {
 export default Post
 
 function PostList({post}) {
+  console.log('test',post)
   return(
     <View style={{ padding: 15, borderBottomWidth: 1, borderBottomColor: 'gray'}}>
       
       <View>
-        <Text style={{ fontSize: 20, fontWeight: 'bold'}}>{post.item.title}</Text>
-        <Text style={{ color: 'gray', fontSize: 15}}>{post.item.content}</Text>
+        <Text style={{ fontSize: 20, fontWeight: 'bold'}}>{post.item.board_title}</Text>
+        <Text style={{ color: 'gray', fontSize: 15}}>{post.item.board_contents}</Text>
       </View>
       
       <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
         <View style={{ display: 'flex', flexDirection: 'row'}}>
-          <Text style={{ color: 'gray'}}>{post.item.time}</Text>
+          <Text style={{ color: 'gray'}}>{dayjs(post.item.board_date).format('YYYY-MM-DD')}</Text>
           <Text style={{ color: 'gray', paddingHorizontal: 5 }}>|</Text>
-          <Text style={{ color: 'gray'}}>{post.item.author}</Text>
+          <Text style={{ color: 'gray'}}>익명</Text>
         </View>
         <View style={{ display: 'flex', flexDirection: 'row'}}>
           <Text style={{ color: 'red'}}>좋아요 1</Text>
